50e62b5761c4e29eb9d27c3f96c245127e5d3b0b
李亚国 <1075543143@qq.com>
2018-09-30 14:14:32 +0800

    人机对话初级版本

---
+++ XbtChatbot_1/core/wav2pcm.py
@@ -0,0 +1,13 @@
+import os
+from setting import settings
+
+
+def wav_to_pcm(wav_file):
+    # 假设wav_file = "音频文件.wav"
+    # wav_file.spilr(".")得到["音频文件","wav"]拿出第一个结果"音频文件"与".pcm"拼接，得到结果"音频文件.pcm"
+    pcm_file = "%s.pcm" % (wav_file.split(".")[0])
+
+    # 就是此前在cmd窗口中输入命令，这里面就是让Python帮我们在cmd中执行命令
+    os.system("%s -y -i %s -acodec pcm_s16le -f s16le -ac 1 -ar 16000 %s" % (settings.TRANSVERTER, wav_file, pcm_file))
+
+    return pcm_file
